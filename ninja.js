5fc48ce8be47b7a36563219bdab4d9cb93ac3924
Dorian Donoso Ampuero <adonosoaster@gmail.com>
2021-04-11 17:09:05 -0400

    first commit

---
+++ ninja.js
@@ -0,0 +1,27 @@
+// construye la clase ninja
+class Ninja {
+    constructor(nombre, velocidad = 3, salud = 0, fuerza = 3) {
+      this.nombre = nombre;
+      this.salud = salud;
+      this.velocidad = velocidad;
+      this.fuerza = fuerza;
+    }
+    sayName() {
+      console.log(this.nombre);
+    }
+    showStats() {
+      const { nombre, salud, fuerza, velocidad } = this;
+      return {
+        nombre,
+        salud,
+        fuerza,
+        velocidad,
+      };
+    }
+  
+    drinkSake() {
+      this.salud += 10;
+    }
+  }
+  
+ 
